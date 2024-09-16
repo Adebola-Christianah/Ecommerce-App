e230ed60a5b9f26ae85c0c7f22b778578c1882c6
Adebola-Christianah <oguntoyeadebola21@gmail.com>
2024-09-16 12:27:33 +0100

    edited build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,5 @@
-python3.12 -m venv venv
+python3.12 -m  pipvenv shell
 
-# activate the virtual environment
-source venv/bin/activate
+
 python3.12 -m pip install -r requirements.txt
 python3.12 manage.py collectstatic --noinput
