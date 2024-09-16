7c9ea293c02848cafd7ef1dbb7936d1a8093b74e
Adebola-Christianah <oguntoyeadebola21@gmail.com>
2024-09-16 10:14:09 +0100

    switched storage to postgres

--- backend/settings.py
+++ backend/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = 'SECRET KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost','.vercel.app','now.sh']
 
 # Application definition
 
@@ -109,14 +109,16 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT"),
     }
 }
-
 # Database configuration for PostgreSQL (uncomment and configure if needed)
 # DATABASES = {
 #    'default': {
