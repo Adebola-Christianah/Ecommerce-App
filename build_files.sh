b46d779ce0d8efd1c44c166964af6450582c96b9
Adebola-Christianah <oguntoyeadebola21@gmail.com>
2024-09-18 16:55:22 +0100

    latest

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+# Ensure pip is installed
+apt-get update && apt-get install -y python3-pip
 
 # Install dependencies
 pip install -r requirements.txt
 
-# Run Django build commands
+# Run your Django build commands
 python manage.py collectstatic --noinput
 python manage.py migrate
